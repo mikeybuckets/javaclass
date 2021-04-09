abd2a7f630e7b41a5fb292637673ca0a1f0b79b8
mikeybuckets <71187513+mikeybuckets@users.noreply.github.com>
2021-04-09 10:24:28 -0700

    Create 5.19.js

---
+++ 5.19.js
@@ -0,0 +1,24 @@
+// 5.19 Lab
+
+
+import java.util.Scanner;
+
+public class LabProgram {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        int min = in.nextInt();
+        int max = in.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= min && arr[i] <= max) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+}
